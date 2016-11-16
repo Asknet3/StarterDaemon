abffed40cf93a2c481980463c3c128fe641d5855
Giuseppe <giuseppe.cristaudo@gmail.com>
2016-11-16 18:49:47 +0100

    primo deploy

---
+++ StarterDaemon/StartForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace StarterDaemon
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.select = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(80, 98);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(734, 43);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(80, 184);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(734, 43);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(633, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(729, 52);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(85, 23);
+            this.select.TabIndex = 3;
+            this.select.Text = "Select";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status: ";
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(127, 408);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(13, 13);
+            this.lbl_status.TabIndex = 5;
+            this.lbl_status.Text = "0";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(77, 381);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 6;
+            this.btn_send.Text = "send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(401, 250);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 7;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 292);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(737, 63);
+            this.textBox2.TabIndex = 8;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 437);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "StartForm";
+            this.Text = "StartForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_status;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_connect;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ StarterDaemon/StartForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarterDaemon
+{
+    public partial class StartForm : Form
+    {
+        TcpListener listener;
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public string text_to_send;
+        const int PORT_NO = 5000;
+
+
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select the file to be executed";
+            if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                openFileDialog1.FileName = "";
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)  // Start Server
+        {
+            listener = new TcpListener(IPAddress.Any, PORT_NO);
+            listener.Start();
+            client =  listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync();                       // Start receiving data in background     
+            backgroundWorker2.WorkerSupportsCancellation = true;      // Ability to cancel this thread
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) //receive data
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+
+                    if (!string.IsNullOrEmpty(receive))
+                    {
+                        Process.Start(@"C:\WebSite\CoffeeBreakWebApp\Asset\autoplaySpotify.vbs");
+                    }
+
+                    this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("You: " + receive + "\n"); }));
+                    receive = "";
+
+                    listener.Stop();
+                    listener = new TcpListener(IPAddress.Any, PORT_NO);
+                    listener.Start();
+                    client = listener.AcceptTcpClient();
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+
+                }
+                catch(Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+            }
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)   // Send Button
+        {
+            if (textBox2.Text != "")
+            {
+                text_to_send = textBox2.Text;
+                lbl_status.Text = text_to_send;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBox2.Text = "";
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e) // Connect to Server
+        {
+            client = new TcpClient();
+            IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT_NO);
+
+            try
+            {
+                client.Connect(IP_End);
+                if (client.Connected)
+                {
+                    lbl_status.Text = "Connected to Server" + "\n";
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync(); // Start receiving data in background 
+                }
+                
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)  // send data
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(text_to_send);
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Me: " + text_to_send + "\n"); }));
+            }
+            else
+            {
+                MessageBox.Show("Send failed!");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            client.Close(); ;
+        }
+    }
+}
